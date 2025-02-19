2d040d2130ab26e408830f5b27cc595db7de345b
schowbey <103155665+schowbey@users.noreply.github.com>
2025-02-18 20:38:40 -0800

    initializing files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage";
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Resume from "./pages/resume";
+import "./App.css";
+  
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/resume" element={<Resume />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/resume.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div>
+      <iframe
+        src="/schowbey-resume.pdf"
+        title="Resume"
+        width="100%"
+        height="1000px"
+        style={{ border: "none" }}
+      ></iframe>
+    </div>
+  );
+}
+
+export default Resume;
